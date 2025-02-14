d95467d4dae7aba05576ccb23e6ea60c57d249e9
Steven <steven_thielemann@yahoo.de>
2025-02-14 17:27:17 +0100

    angular-material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(), 
-    provideRouter(routes)
+    provideRouter(routes), 
+    provideAnimationsAsync()
   ]
 };
